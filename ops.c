559c2f0c73a2aac07a0af238b58b90159e6b60c2
LEANDRO AUGUSTO FERREIRA SANTOS <108150412+leandroaugf@users.noreply.github.com>
2023-05-28 18:29:03 -0300

    COMMIT-4

--- ops.c
+++ ops.c
@@ -1,52 +1,57 @@
+#include "ops.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "ops.h"
 
-float soma(float content1, float content2) {
-  return content1 + content2;
+float soma(float a, float b){
+    float result;
+    result = a + b;
+
+    return result;
 }
 
-float sub(float content1, float content2) {
-  return content1 - content2;
+float sub(float a, float b){
+    float result;
+    result = a-+ b;
+
+    return result;
 }
 
-float mult(float content1, float content2) {
+float mult(float RAMContent1, float RAMContent2){
+    int flag = 0;
+    float result = 0;
+
+    if(RAMContent1 < 0) {
+        RAMContent1 = -RAMContent1;
+        flag++;
+    }
+    if (RAMContent2 < 0) {
+        RAMContent2 = -RAMContent2;
+        flag++;
+    }
+
+    for (int i = 0; i < RAMContent2; i++) 
+        result += RAMContent1;
+    if(flag == 1) 
+        result = -result;
+
+    return result;
+}
+
+float divis(float RAMContent1, float RAMContent2) {
   int flag = 0;
   float result = 0;
-
-  if(content1 < 0) {
-    content1 = -content1;
+  if(RAMContent1 < 0) {
+    RAMContent1 = -RAMContent1;
     flag++;
   }
-  if (content2 < 0) {
-    content2 = -content2;
+  if(RAMContent2 < 0) {
+    RAMContent2 = -RAMContent2;
     flag++;
   }
-
-  for (int i = 0; i < content2; i++) 
-    result += content1;
-  
-  if(flag == 1) {
-    result = -result;
-  }
-  return result;
-}
-
-float div(float a, float b) {
-  int flag = 0;
-  float result = 0;
-  if(a < 0) {
-    a = -a;
-    flag++;
-  }
-  if(b < 0) {
-    b = -b;
-    flag++;
-  }
-  int aux = b;
-  while(a >= b){ 
-      b += aux;
-      result++;
+  int aux = RAMContent2;
+  while(RAMContent1 >= RAMContent2) { 
+    RAMContent2 += aux;
+    result++;
   }
   if (flag == 1)
     result = -result;
@@ -54,43 +59,69 @@ float div(float a, float b) {
   return result;
 }
 
-float fibonacci(int n) {
-  int i = 1, k;
-  float result = 0; 
-  for (k = 1; k <= n; k++) {
-    result += i;
-    i = result - i;
+float fibonacci(int num) {
+    if(num == 0)
+        return 0; 
+    if(num == 1)
+        return 1;
+    
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+float exponencial(float RAMContent1, float RAMContent2) {
+    float aux = RAMContent1;
+    float result = RAMContent1;
+    // 10 3
+    if(RAMContent1 < 0 || RAMContent2 < 0)
+        result = -1; //           [ERRO: NUMERO NEGATIVO]
+    if (RAMContent1 > 64)
+        result = -2; //            [ERRO: NUMERO > LIMITE]
+    for (int i = 0; i < RAMContent2; i++) 
+        result = mult(result, aux);
+
+    return result;
+}
+
+float fatorial(float RAMContent1) {
+    float result = RAMContent1;
+    if (RAMContent1 < 0)
+        return -1; //           [ERRO: NUMERO NEGATIVO]
+    for (int i = RAMContent1; i > 1; i--) 
+        result = mult(result, i - 1);
+
+    return result;
+}
+
+float raiz(float RAMContent1) {
+  float result = 0;
+  float valorprox;
+  float valormax = RAMContent1;
+  
+  int raizaux = RAMContent1;
+
+  for (int i=2;;i++) {
+      raizaux = divis(RAMContent1,i); 
+
+      if (exponencial(raizaux, 2) == RAMContent1)
+      return raizaux;
+      else if (exponencial(raizaux, 2) < RAMContent1) { 
+          for (int i = raizaux; i <= valormax; i++) {
+          result = exponencial(i, 2);
+          if(result >= RAMContent1) {
+              valorprox = valorprox - RAMContent1;
+              result = result - RAMContent1;
+              result = (result < 0)? -result : result;
+              valorprox = (valorprox < 0)? -valorprox : valorprox;
+              if (valorprox < result)
+                  return i - 1;
+              return i;
+          }
+          valorprox = result;
+          }
+      }
+      else 
+        valormax=raizaux;
   }
-  
+
   return result;
-}
-
-float expo(float n1, float n2) {
-  float expo = 0;
-  if(n1 < 0 || n2 < 0)
-    return -1; //           [ERRO: NUMERO NEGATIVO]
-  if (n1 > 64)
-    return -2; //            [ERRO: NUMERO > LIMITE]
-  
-  for (int i = 0; i < n2; i++) 
-    expo += mult(n1, n1);
-
-  return expo;
-}
-
-float fat(int num) {
-  float fact = num;
-  if (num < 0)
-    return -1; //           [ERRO: NUMERO NEGATIVO]
-  for (int i = num; i > 1; i--) 
-    fact = mult(fact, i - 1);
-
-  return fact;
-} 
-
-float raiz(int num) {
-  float sqroot = 0;
-  
-  
-  return sqroot;
 }
--- ops.h
+++ ops.h
@@ -1,13 +1,13 @@
 #ifndef OPS_H
 #define OPS_H
 
-float raiz(int);
-float fat(int);
-float fibonacci(int);
-float div(float, float;
-float expo(float, float);
-float sub(float, float);
-float mult(float, float);
-float soma(float, float);
+float soma(float a, float b);
+float sub(float a, float b);
+float mult(float RAMContent1, float RAMContent2);
+float divis(float RAMContent1, float RAMContent2);
+float fibonacci(int num);
+float exponencial(float RAMContent1, float RAMContent2);
+float fatorial(float RAMContent1);
+float raiz(float RAMContent1);
 
 #endif
