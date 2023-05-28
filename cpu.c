fda03a498a11e15c5b3445564e733a238bb9a180
kaka27022 <117757580+kaka27022@users.noreply.github.com>
2023-05-28 16:00:18 -0300

    Add files via upload

--- cpu.c
+++ cpu.c
@@ -1,7 +1,4 @@
 #include "cpu.h"
-#include "ops.c"
-#include "ops.h"
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,7 +7,7 @@ void start(Machine* machine, Instruction* instructions, int RAMSize) {
     machine->RAM.items = (float*) malloc(sizeof(float) * RAMSize);
     machine->RAM.size = RAMSize;
     for (int i=0;i<RAMSize;i++)
-        machine->RAM.items[i] = (float)rand() / RAND_MAX;
+        machine->RAM.items[i] = rand()%10;
 }
 
 void stop(Machine* machine) {
@@ -18,110 +15,154 @@ void stop(Machine* machine) {
     free(machine->RAM.items);
 }
 
-void run(Machine* machine) { //                                                                                                   [FUNCAO ONDE O TUDO EH EXECUTADO]
-    
-    int opcode = 0;
+float soma(float a, float b){
+
+    float result;
+    result = a + b;
+    return result;
+
+}
+
+float sub(float a, float b){
+
+    float result;
+    result = a-+ b;
+    return result;
+}
+
+float multi(float a, float b){
+
+    float result = 0;
+    int confere_negativo = 0;
+
+    if((a < 0 && b > 0) || (a > 0 && b < 0)){
+        confere_negativo = 1;
+    }
+
+    if(a < 0){
+        a = -a;
+    }
+
+    if(b < 0){
+        b = -b;
+    }
+
+    for(int i = 0; i < b; i++){
+        result += a;
+    }
+
+    if(confere_negativo){
+        result = -result;
+    }
+
+    return result;
+}
+
+float divi(float a, float b){   //a = dividendo  b = divisor
+
+    float result = 0;
+    int aux_divisao = b;
+    int confere_negativo = 0;
+
+    if((a < 0 && b > 0) || (a > 0 && b < 0)){
+        confere_negativo = 1;
+    }
+
+    if(a < 0){
+        a = -a;
+    }
+
+    if(b < 0){
+        b = -b;
+    }
+
+    while(a >= b){
+        b += aux_divisao;
+        result++;
+    }
+
+    if(confere_negativo){
+        result = - result;
+    }
+
+    return result;
+
+}
+
+
+
+void run(Machine* machine) {
+    // Registradores
     int PC = 0; // Program Counter - Contador de programa
+    int opcode = 0;
     int address1;
     int address2;
-    int address3;
-    float raizes[15] = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225};
-
     float value;
     float result;
     float RAMContent1;
     float RAMContent2;
-    srand(time(NULL));
-
+    int address3;
     while(opcode != -1) {
         Instruction instruction = machine->instructions[PC];
         opcode = instruction.opcode;
-//      [ATRIBUICAO DOS VALORES AAS VARIAVEIS]
-        address1 = instruction.info1;
-        address2 = instruction.info2;
-        address3 = instruction.info3;
-        RAMContent1 = machine->RAM.items[address1];
-        RAMContent2 = machine->RAM.items[address2];
-
         switch (opcode) {
             case -1:
                 printf("  > Finalizando a execucao.\n");
                 break;
-
-            case 0: // [DONE]
+            case 0: // Levando informação para a RAM
                 value = (float) instruction.info1;
                 address1 = instruction.info2;
                 machine->RAM.items[address1] = value;
                 printf("  > Levando informacao (%f) para a RAM[%d].\n", value, address1);
                 break;
-
-            case 1: // [SOMA]
+            case 1: // Somando
+                address1 = instruction.info1;
+                address2 = instruction.info2;
+                RAMContent1 = machine->RAM.items[address1];
+                RAMContent2 = machine->RAM.items[address2];
                 result = soma(RAMContent1, RAMContent2);
-                
+                address3 = instruction.info3;
                 machine->RAM.items[address3] = result;
                 printf("  > Somando RAM[%d] (%f) com RAM[%d] (%f) e salvando na RAM[%d] (%f).\n", 
                                 address1, RAMContent1, address2, RAMContent2, address3, result);
                 break;
-
-            case 2: // [SUBTRACAO]
+            case 2: // Subtraindo
+                address1 = instruction.info1;
+                address2 = instruction.info2;
+                RAMContent1 = machine->RAM.items[address1];
+                RAMContent2 = machine->RAM.items[address2];
                 result = sub(RAMContent1, RAMContent2);
-                
+                address3 = instruction.info3;
                 machine->RAM.items[address3] = result;
-                printf("  > Subtraindo RAM[%d] (%f) com RAM[%d] (%f) e salvando na RAM[%d] (%f).\n", //
+                printf("  > Subtraindo RAM[%d] (%f) com RAM[%d] (%f) e salvando na RAM[%d] (%f).\n", 
                                 address1, RAMContent1, address2, RAMContent2, address3, result);
                 break;
 
-            case 3: // multiplicando
-                RAMContent1 = 36;
-                RAMContent2 = rand() % 10 + 1;
-                result = mult(RAMContent1, RAMContent2);
+            case 3: // Multiplicando
+                address1 = instruction.info1;
+                address2 = instruction.info2;
+                RAMContent1 = machine->RAM.items[address1];
+                RAMContent2 = machine->RAM.items[address2];
+                result = multi(RAMContent1, RAMContent2);
+                address3 = instruction.info3;
                 machine->RAM.items[address3] = result;
-                printf("  > Multiplicando RAM[%d] (%.2f) com RAM[%d] (%f) e salvando na RAM[%d] (%f).\n",
+                printf("  > Multiplicando RAM[%d] (%f) com RAM[%d] (%f) e salvando na RAM[%d] (%f).\n", 
                                 address1, RAMContent1, address2, RAMContent2, address3, result);
                 break;
 
-            case 4: // [DIVISAO]
-                RAMContent1 = 36.0; 
-                RAMContent2 = 9.0;
-                result = div(RAMContent1, RAMContent2);
+            case 4:  // Dividindo
+                address1 = instruction.info1;
+                address2 = instruction.info2;
+                RAMContent1 = machine->RAM.items[address1];
+                RAMContent2 = machine->RAM.items[address2];
+                result = divi(RAMContent1, RAMContent2);
+                address3 = instruction.info3;
                 machine->RAM.items[address3] = result;
-                printf("  > Dividindo RAM[%d] (%f) com RAM[%d] (%f) e salvando na RAM[%d] (%f).\n",
-                            address1, RAMContent1, address2, RAMContent2, address3, result);
+                printf("  > Dividindo RAM[%d] (%f) com RAM[%d] (%f) e salvando na RAM[%d] (%f).\n", 
+                                address1, RAMContent1, address2, RAMContent2, address3, result);
                 break;
 
-            case 5: // [FIBONACCI]
-                RAMContent1 = rand() % 15 + 1;
-                result = fibonacci(RAMContent1);
-                machine->RAM.items[address3] = result;
-                printf("  > Calculando a sequência de Fibonacci: RAM[%d] (%f) e salvando na RAM[%d] (%f).\n.",
-                            address1, RAMContent1, address3, result);
-                break;
-
-            case 6: // [EXPONENCIAL]
-                RAMContent1 = rand() % 20 + 1;
-                RAMContent2 = rand() % 3 + 1;
-                result = expo(RAMContent1, RAMContent2);
-                machine->RAM.items[address3] = result;
-                printf("  > Calculando o exponencial de RAM[%d] (%f) com RAM[%d] (%f) e salvando na RAM[%d] (%f).\n",
-                            address1, RAMContent1, address2, RAMContent2, address3, result);
-                break; 
-
-            case 7: // [FATORIAL]
-                RAMContent1 = rand() % 10 + 1;
-                result = fat(RAMContent1);
-                machine->RAM.items[address3] = result;
-                printf("  > Calculando o fatorial de RAM[%d] (%f) e salvando na RAM[%d] (%f).\n",
-                            address1, RAMContent1, address3, result);
-                break;
-            case 8: // [RAIZ]
-                RAMContent1 = raizes[random() % 15 + 1];
-                result = raiz(RAMContent1);
-                machine->RAM.items[address3] = result;
-                printf("  > Calculando a raiz quadrada de RAM[%d] (%f) e salvando na RAM[%d] (%f).\n",
-                            address1, RAMContent1, address3, result);
-                break;
-                // [MDC]
-        }   
+        }
         PC++;
     }
 }
